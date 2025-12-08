b970285230c57d9225859c4abe82bf9b8b938682
somefood <iamseokjuhong@gmail.com>
2025-12-08 22:52:50 +0900

    애플리케이션 System.exit(SpringApplication.exit())로 래핑
    배치의 작업 성공/실패 상태를 exit code로 외부 시스템에 전달할 수 있어 배치 모니터링과 제어에 필수적이라 아래와 같이 exit로 감싸는걸 권장

--- kill-batch-system-boot/src/main/java/com/system/batch/KillBatchSystemApplication.java
+++ kill-batch-system-boot/src/main/java/com/system/batch/KillBatchSystemApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class KillBatchSystemApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(KillBatchSystemApplication.class, args);
+        // 배치의 작업 성공/실패 상태를 exit code로 외부 시스템에 전달할 수 있어 배치 모니터링과 제어에 필수적이라 아래와 같이 exit로 감싸는걸 권장
+		System.exit(SpringApplication.exit(SpringApplication.run(KillBatchSystemApplication.class, args)));
 	}
 
 }
